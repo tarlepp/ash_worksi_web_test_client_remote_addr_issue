ed621eaac9b631b41bc6ad78d090e8fef51eaa47
Tarmo Leppänen <tarmo.leppanen@pinja.com>
2023-02-24 21:53:07 +0200

    Some solution to make that work

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -10,6 +10,9 @@ class IndexController
     #[Route(path: '/')]
     public function __invoke(Request $request): JsonResponse
     {
-        return new JsonResponse(['clientIp' => $request->getClientIp()]);
+        // This is a bit hacky, but you could do this in request subscriber where you listen `RequestEvent::class`
+        $_SERVER['REMOTE_ADDR'] = $request->getClientIp();
+
+        return new JsonResponse(['clientIp' => $_SERVER['REMOTE_ADDR']]);
     }
 }
